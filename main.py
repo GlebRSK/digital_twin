eb1244f8f350963035e7bead20e4fa0160dc13c2
Glebrctu <gleb172v@gmail.com>
2020-10-13 00:44:08 +0300

    add comment

--- main.py
+++ main.py
@@ -7,49 +7,68 @@ import server_data as serv
 import time
 
 
-def get_apparat_data(app_name='', need_graph=False):
+'''
+запрос к серверу для получения данных: 
+app_name - по умолчанию пустая строка, в этом случае выводится список возможных аппаратов(элементов)
+need_graph - по умолчанию False, при True строится график в реальном времени
+apparats_info - список данных, полученных из запроса
+correct_app - в случае неверного имени параметра выводит ошибку
+'''
+def get_apparat_data(app_name='', need_graph=False): 
 
     apparats_info = requests.get(url=serv.URL, headers=serv.HEADERS,
                                  auth=HTTPBasicAuth('apikey', serv.AUTH)).json()
 
     attempts_count = 0
-    while not apparats_info and attempts_count < 100:
+    #при проблемах на сервере повторяется попытка запроса и вывдится соответствующая информация
+    while not apparats_info and attempts_count <= 100:
         print('Not returning a 200 response, try again')
         time.sleep(5)
         apparats_info = requests.get(url=serv.URL, headers=serv.HEADERS,
                                      auth=HTTPBasicAuth('apikey', serv.AUTH)).json()
         attempts_count += 1
 
+    if attempts_count >= 100:
+        print('Server error. Check URL or auth')        
+        return 0        
+
     correct_app = False
+    #если отсутствует имя аппарата, выводится список имен
     if app_name != '':
-        if need_graph:
+        if need_graph: #если требуется построение графика
+
             for data in apparats_info:
                 if data['tag'] == app_name:
                     correct_app = True
-                    with open(f'{app_name}.txt', 'a') as f:
+                    with open(f'{app_name}.txt', 'a') as f: #запись данных в файл
                         f.writelines(str(data['value']) + '\n')
                     return float(data['value'])
-            if not correct_app:
+
+            if not correct_app: # при вводе неправильного значения имени элемента
                 print('wrong app name')
                 return 0
-        else:
-            while True:
+
+        else: # если не требуется построение графика
+            while True: #бесконечный цикл запросов к серверу с записью данных в файл
+
                 for data in apparats_info:
                     if data['tag'] == app_name:
                         correct_app = True
                         print(data['value'])
                         with open(f'{app_name}.txt', 'a') as f:
                             f.writelines(str(data['value']) + '\n')
+
                 if not correct_app:
                     print('wrong app name')
                     return 0
                 time.sleep(1)
+
     else:
         for apparat in apparats_info:
             print(apparat['tag'])
 
 
-def create_plot(app=''):
+def create_plot(app=''): #получение графика
     if app == '':
         print('Select apparat name')
         return 0
