73d2ffb78cb48baedda2725c7a84d0469c3675f4
glebrsk <glebrsk@git.com>
2022-05-21 17:43:26 +0300

    remove some information

--- server_data.py
+++ server_data.py
@@ -1,7 +1,5 @@
-URL = 'http://193.232.56.4:8010/scheme/290758cb-8035-4b30-a7b0-5facfde12c1f/variables/read/all/'
-#URL = 'http://193.232.56.4:8010/scheme/3edfa94c-03b9-4871-89cb-06a59ebd917a/variables/read/all/'
+URL = 'url'
 HEADERS = {"Accept": "application/json"}
-AUTH = '226d835cb3c620908c41c851b8c9b9da48dca669eeefc88acb'
-#AUTH = HTTPBasicAuth('apikey', '966e87af4e9639772015591a6044311a23868b60db6ff5aad6')
+AUTH = 'auth_key'
 TIME_LIMIT = 500 #предел считывания информации в секундах
 TIME_INTERVAL = 1000 #интервал запросов в милисекундах
